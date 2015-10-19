8a2f56e10a0d69bd93989a022b143ddc7abd862c
droglenc <dogle@northland.edu>
2015-10-19 09:29:43 -0500

    Converted all txt to csv, deleted txt files in data-raw

--- data-raw/aaaConvertTab2CSV.R
+++ data-raw/aaaConvertTab2CSV.R
@@ -1,1 +1,29 @@
-aaaConvertTab2CSV.R
+##############################################################
+##                                                          ##
+## Use this to convert tab-delimited text files (*.txt) to  ##
+##   comma separated values files (*.csv) in the data-raw   ##
+##   directory of the FSAdata package.                      ##
+##                                                          ##
+##############################################################
+
+library(tools)
+
+## Get all .txt files in the data-raw directory
+setwd("C:/aaaWork/Programs/GitHub/FSAdata/data-raw")
+raw <- list.files(pattern="*.txt")
+
+## Needed to handle CutthroatALF.txt specially
+## This is specific for the CutthroatALf.txt file
+tmp <- read.table("CutthroatALf.txt",header=TRUE,
+                   colClasses=c("character","numeric"))
+write.csv(tmp,"CutthroatALf.csv",quote=FALSE,row.names=FALSE)
+raw <- raw[raw!="CutthroatALf.txt"]
+
+## Cycle through each file to make it a CSV file
+for (f in raw) {
+  print(f)
+  ## Read file
+  tmp <- read.table(f,header=TRUE)
+  ## Write out as a CSV file
+  write.csv(tmp,paste0(file_path_sans_ext(f),".csv"),quote=FALSE,row.names=FALSE)
+}
